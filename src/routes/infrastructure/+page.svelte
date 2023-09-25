91b2e3089d9740361d937c643a07e50e721ff3f8
Megakuul <megakuul@gmx.ch>
2023-09-25 18:24:26 +0200

    Changed vendor-lock warning

--- src/routes/infrastructure/+page.svelte
+++ src/routes/infrastructure/+page.svelte
@@ -106,8 +106,8 @@ All backup strategies follow a strict standard and have a specific function, do 
 {#if vendorLock && !hideVendorLockWarning}
 <div transition:fade class="alert alert-warning z-50 w-10/12 grid place-self-center fixed bottom-5 text-xs sm:text-sm">
     <svg xmlns="http://www.w3.org/2000/svg" class="stroke-current shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z" /></svg>
-    <span>Warning: You only use cloud backups, if you do use the same cloudprovider for all backups, 
-        remember that you are vendor locked, if something happens to the cloudprovider you lose all backups</span>
+    <span>Caution: Using only cloud backups with a single provider can lead to vendor lock-in.
+        If issues arise with that provider, you risk losing all backups.</span>
     <button class="btn btn-ghost btn-sm" on:click={() => {vendorLock = false; hideVendorLockWarning = true;}}>X</button>
 </div>
 {/if}
